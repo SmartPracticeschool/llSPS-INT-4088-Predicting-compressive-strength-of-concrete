eff7cd8b5d786019f30180f85d12f38e5a1a6726
Aniket Ulpulwar <57874870+aniket06081999@users.noreply.github.com>
2020-11-08 21:01:24 +0530

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,39 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('linear.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    X_test = [[float(X) for X in request.form.values()]]
+    
+    
+    prediction = model.predict(X_test)
+    print(prediction)
+    output=prediction[0]
+    return render_template('index.html', prediction_text='Compressive Strength of Concrete kg/m^3 {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.y_pred_list([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
